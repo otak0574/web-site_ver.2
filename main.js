59581f18feb8ee6d6e8dd6e731a24b05cb79a98f
otak0574 <159395656+hitisou-yuen@users.noreply.github.com>
2026-05-28 08:12:03 +0900

    Update main.js

--- main.js
+++ main.js
@@ -45,6 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     <li><a href="dogrun">ドッグラン</a></li>
                     <li><a href="story">こだわりとストーリー</a></li>
                     <li><a href="access">アクセス</a></li>
+                    <li><a href="bento">夜専用うなぎ弁当</a></li>
                     <li style="margin-top: 24px; border-top: 1px dashed var(--color-main); padding-top: 24px;"><a href="simulation.html" style="color: var(--color-accent); font-weight: bold;">料金シミュレーション</a></li>
                 </ul>
             </nav>
