5e1bbec3607f74e69aacff6a4df5c0f94a8767ef
otak0574 <159395656+hitisou-yuen@users.noreply.github.com>
2026-07-31 07:28:14 +0900

    鮎の掴み取りページ追加

--- main.js
+++ main.js
@@ -45,7 +45,6 @@ document.addEventListener('DOMContentLoaded', () => {
                     <li><a href="dogrun">ドッグラン</a></li>
                     <li><a href="story">こだわりとストーリー</a></li>
                     <li><a href="access">アクセス</a></li>
-                    <li style="margin-top: 24px; border-top: 1px dashed var(--color-main); padding-top: 24px;"><a href="simulation.html" style="color: var(--color-accent); font-weight: bold;">料金シミュレーション</a></li>
                 </ul>
             </nav>
         </header>
